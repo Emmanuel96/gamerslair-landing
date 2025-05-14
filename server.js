d6a07e46e38128e9432b0209f0840d398fda6f2c
Don-Par <dannynduka66@gmail.com>
2025-05-14 01:35:00 -0230

    Handle all HTTP methods for static site

--- server.js
+++ server.js
@@ -4,8 +4,14 @@ const app = express();
 
 app.use(express.static(__dirname));
 
+// Handle GET requests for all routes
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Handle all other HTTP methods
+app.all('*', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
